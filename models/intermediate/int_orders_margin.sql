f88bd2c506c532b9749466da605e2c59144d654c
Kevin <houngkevin@gmail.com>
2025-10-22 08:38:08 +0000

    orders margin using int sales margin join

---
+++ models/intermediate/int_orders_margin.sql
@@ -0,0 +1,12 @@
+with margin as (
+    select * from {{ref('int_sales_margin')}}
+)
+select
+    margin.orders_id as orders_id,
+    margin.date_date as date_date,
+    ROUND(SUM(margin.revenue),2) as revenue,
+    SUM(margin.quantity) as quantity,
+    ROUND(SUM(margin.purchase_cost),2) as purchase_cost,
+    ROUND(SUM(margin.margin),2) as margin
+    from margin
+    group by orders_id, date_date
