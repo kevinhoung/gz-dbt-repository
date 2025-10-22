18e9bc100aa36d68a84a4c3def48b9870dfe1a8f
Kevin <houngkevin@gmail.com>
2025-10-22 05:49:31 +0000

    add surrogate key

--- models/staging/raw/stg_raw__sales.sql
+++ models/staging/raw/stg_raw__sales.sql
@@ -12,8 +12,9 @@ renamed as (
 
     select
         date_date,
+        orders_id || '-' || pdt_id as surrogate_key,
         orders_id,
-        pdt_id as product_id,
+        pdt_id as products_id,
         revenue,
         quantity
 
