7cd490610421ab4111e1c2a5d97eacdfb22c29ad
Kevin <houngkevin@gmail.com>
2025-10-22 04:42:08 +0000

    transforming tables to proper data types and fixing column names

---
+++ models/staging/raw/stg_raw__ship.sql
@@ -0,0 +1,24 @@
+-- stg_raw__ship.sql
+
+
+with 
+
+source as (
+
+    select * from {{ source('raw', 'ship') }}
+
+),
+
+renamed as (
+
+    select
+        orders_id,
+        shipping_fee,
+        logcost,
+        cast(ship_cost as float64) as ship_cost
+
+    from source
+
+)
+
+select * from renamed
