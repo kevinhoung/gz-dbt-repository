c857b2a9d790023649b72f0dca6e8f66063a26c8
Kevin <houngkevin@gmail.com>
2025-10-23 05:04:45 +0000

    alias for int-campaigns model

--- models/mart/marketing/int_campaigns_day.sql
+++ models/mart/marketing/int_campaigns_day.sql
@@ -1,7 +1,7 @@
 select
-    date_date,
-    SUM(ads_cost),
-    SUM(impression),
-    SUM(click)
+    date_date as date_date,
+    SUM(ads_cost) as ads_cost,
+    SUM(impression) as impression,
+    SUM(click) as click
     from {{ref('int_orders_operational')}}
 group by date_date
