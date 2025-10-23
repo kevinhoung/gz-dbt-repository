7d4b8b2e8d04d2a4931ddef37cd09aa6d52dde43
Kevin <houngkevin@gmail.com>
2025-10-23 07:23:44 +0000

    using dbil utils relations pckage

--- models/intermediate/int_campaigns.sql
+++ models/intermediate/int_campaigns.sql
@@ -1,5 +1,5 @@
 -- int_campaigns.sql
-
+/*
 select * from {{ref('stg_raw__adwords')}}
 UNION ALL
 select * from {{ref('stg_raw__bing')}}
@@ -7,3 +7,14 @@ UNION ALL
 select * from {{ref('stg_raw__criteo')}}
 UNION ALL
 select * from {{ref('stg_raw__facebook')}}
+*/
+
+{{ dbt_utils.union_relations(
+    relations = [
+        ref('stg_raw__adwords'),
+        ref('stg_raw__bing'),
+        ref('stg_raw__criteo'),
+        ref('stg_raw__facebook')
+    ],
+     source_column_name = None
+) }}
