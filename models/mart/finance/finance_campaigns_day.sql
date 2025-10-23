e89758840a02c8d333d87582341df9dc31f9837d
Kevin <houngkevin@gmail.com>
2025-10-23 06:35:12 +0000

    finally challenge 3 is finished

--- models/mart/finance/finance_campaigns_day.sql
+++ models/mart/finance/finance_campaigns_day.sql
@@ -2,23 +2,29 @@
 
 with marketing as (
     select * from {{ref('int_campaigns_day')}}
+),
+finance as (
+    select * from {{ref('finance_days')}}
+),
+financemarketingcombine as (
+    select * from finance
+    left join marketing
+    using (date_date)
 )
 select
-    finance.date_date,
-    SUM(finance.op_margin - marketing.ads_cost) as ads_margin,
-    SUM(finance.average_basket) average_basket,
-    SUM(finance.op_margin) as operational_margin,
-    SUM(marketing.ads_cost) as ads_cost,
-    SUM(marketing.impression) as ads_impression,
-    SUM(marketing.click) as ads_clicks,
-    SUM(finance.quantity_sold) as quantity,
-    SUM(finance.revenue) as revenue,
-    SUM(finance.total_purchase_cost) as purchase_cost,
-    SUM(finance.revenue) - SUM(finance.total_purchase_cost) as margin,
-    SUM(finance.total_shipping_fee) as shipping_fee,
-    SUM(finance.total_log_cost) as log_cost,
-    SUM(finance.total_ship_cost) as ship_cost
-    from {{ref('finance_days')}} as finance
-left join marketing
-using (date_date)
+    date_date,
+    SUM(op_margin - ads_cost) as ads_margin,
+    SUM(average_basket) average_basket,
+    SUM(op_margin) as operational_margin,
+    SUM(ads_cost) as ads_cost,
+    SUM(impression) as ads_impression,
+    SUM(click) as ads_clicks,
+    SUM(quantity_sold) as quantity,
+    SUM(revenue) as revenue,
+    SUM(total_purchase_cost) as purchase_cost,
+    SUM(revenue) - SUM(total_purchase_cost) as margin,
+    SUM(total_shipping_fee) as shipping_fee,
+    SUM(total_log_cost) as log_cost,
+    SUM(total_ship_cost) as ship_cost
+    from financemarketingcombine
 group by date_date
